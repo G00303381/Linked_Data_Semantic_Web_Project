b52cbdc89175047b5f9f826e7e6676c0c5d7a675
G00303381 <danielmaloneygti@gmail.com>
2015-11-22 14:44:06 +0000

    Added open/close connection handeling

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ var express = require('express');
 var sqlite3 = require('sqlite3').verbose();
 //Install body parser to parse the data from the body
 var bodyParser = require('body-parser');
-var fs = require('fs');
 
 //Create a HTTP server app
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+//Start the server.
+var server = app.listen(8080);
 
 // import file system
 var fs = require('fs');
@@ -19,7 +20,8 @@ var file2 = "population.db"
 var existsC = fs.existsSync(file);
 var existsP = fs.existsSync(file2);
 
-//Read in the CSV files and convert them into JSON formatted data
+//Read in the CSV files and convert them into JSON
+//formatted data
 var crimes = JSON.parse(fs.readFileSync('crime_offences.json', 'utf8'));
 var population = JSON.parse(fs.readFileSync('population.json', 'utf8'));
 
@@ -35,17 +37,19 @@ if(!existsP) {
   fs.openSync(file2, "w");
 }
 
-//Set up a databse using SQLite3 using files
+//Set up a databse using SQLite3 using the imported files
 var dbC = new sqlite3.Database(file);
 var dbP = new sqlite3.Database(file2);
 
-//Create the crime_offences database and populate the table with the JSON files
+// Create the crime_offences database and populate the
+// table with the JSON files
 dbC.serialize(function() {
+    // check to see of the database file already exists
     if(!existsC) {
+        //if the table doesn't exist continue to create the database and corresponding columns
         dbC.run('CREATE TABLE crime_offences(id INTEGER PRIMARY KEY AUTOINCREMENT, GardaStation Text, Crime Text, '
                + 'Y2006Q1 INTEGER, Y2006Q2 INTEGER, Y2006Q3 INTEGER, Y2006Q4 INTEGER, Y2011Q1 INTEGER, '
                + 'Y2011Q2 INTEGER, Y2011Q3 INTEGER, Y2011Q4 INTEGER)');
-    
         var stmt = dbC.prepare('INSERT INTO crime_offences '
                               +'(GardaStation,Crime,Y2006Q1,Y2006Q2,Y2006Q3,Y2006Q4,Y2011Q1, '
                               +'Y2011Q2,Y2011Q3,Y2011Q4) VALUES (?,?,?,?,?,?,?,?,?,?)');
@@ -62,10 +66,13 @@ dbC.serialize(function() {
 
 //Create the population database and populate the table with the JSON files
 dbP.serialize(function () {
-   if(!existsP) {
+    //check to see if the databse has already been created
+    if(!existsP) {
+        //if the table doesn't exist contnue to create the table and corresponding columns
        dbP.run('CREATE TABLE population(id INTEGER PRIMARY KEY AUTOINCREMENT, Sex Text, Y2006 INTEGER, Y2011 INTEGER, City Text)');
        var stmt = dbP.prepare('INSERT INTO population (Sex, Y2006, Y2011, City) VALUES (?,?,?,?)');
        
+        //For each loop to populate the databse with the JSON components
        population.forEach(function (fill) {
            stmt.run(fill.Sex, fill.Y2006, fill.Y2011, fill.City);
        });
@@ -74,51 +81,68 @@ dbP.serialize(function () {
    }         
 });
 
+dbC.close();
+dbP.close();
+
 //When a user goes to /, return a welcome string
 app.get('/', function(req, res) {
-    res.render('Crime.ejs');
-    //res.send("Welcome to the api.");
+    res.send("Welcome to the api.");
     console.log("Port 8080: Something is Happening!");
 });
 
 //When a user goes to /allp, return the population database entirely (tab formatted).
 app.get('/allp', function(req, res){
+    var dbP = new sqlite3.Database(file2);
     console.log("Retrieving population data... ");
     dbP.all("SELECT * FROM population", function(err, row) {
     rowString = JSON.stringify(row, null, '\t');
     res.sendStatus(rowString);
   });
+    dbP.close();
 });
 
 //When a user goes to /allc, return the population database (tab formatted).
 app.get('/allc', function(req, res) {
+    var dbC = new sqlite3.Database(file);
     console.log("Retrieving crime data... ");
     dbC.all("SELECT * FROM crime_offences", function(err, row) {
     rowString = JSON.stringify(row, null, '\t');
     res.sendStatus(rowString);
   });
+    dbC.close();
 });
 
-//Query the database using parameters entered by the user to find data specific information about a particulr crime offence.
+app.get('/offenceId/:id', function(req, res) {
+    var dbC = new sqlite3.Database(file);
+    db.all("SELECT id, GardaStation, Crime FROM crime_offences")
+    rowstring = JSON.stringify(row, null, '\t');
+    dbC.close();
+});
+
+//Query the crimes DB using parameters entered by the user to find data specific information about a particulr crime offence.
 app.get('/crimesbyoffence/:offence', function(req, res) {
-    console.log("Getting strings like...");
+    var dbC = new sqlite3.Database(file);
+    console.log("Using string " + req.params.offence + " to query the database");
     dbC.all("SELECT * FROM crime_offences WHERE Crime LIKE \"%"+ req.params.offence + "%\"", function(err, row) { 
         rowString = JSON.stringify(row, null, '\t');
         res.sendStatus(rowString);
-        console.log(req.params.offence);
     });
+    dbC.close();
 });
 
+//Query the population DB using parameters entered by the user to find data specific information about population size by sex
 app.get('/populationbysex/:sex', function (req, res) {
+    var dbP = new sqlite3.Database(file2);
+    console.log("Using string " + req.params.sex + " to query the database");
     dbP.all("SELECT * FROM population WHERE Sex LIKE \"%" + req.params.sex + "%\"", function(err,row)
     {
         var rowString = JSON.stringify(row, null, '\t');
         res.sendStatus(rowString);
-        console.log(req.params.yearStr);
     });
+    dbP.close();
 });
 
-//Crime Rates and total population by City
+//Query to compare Crime Rates and total population by City
 app.get('/compare/:offence/:city', function (req, res)
 {
     dbP.all("SELECT DISTINCT crime_offences.GardaStation as Station, crime_offences.Crime as Crime, (crime_offences.Y2006Q1 + crime_offences.Y2006Q2 + crime_offences.Y2006Q3 + crime_offences.Y2006Q4 + crime_offences.Y2011Q1 + crime_offences.Y2011Q2 + crime_offences.Y2011Q3 + crime_offences.Y2011Q4) AS Total_Crimes, (population.Y2006 + population.Y2011) AS Total_Population, population.City as City FROM crime_offences LEFT JOIN population WHERE crime_offences.Crime LIKE \"%"+req.params.offence+"%\" AND population.City LIKE \"%"+req.params.city+"%\" ", function(err,row)
@@ -128,29 +152,37 @@ app.get('/compare/:offence/:city', function (req, res)
     });
 });
 
+//Query to delete a particualr record in the Crimes db by specific id
 app.delete('/deleteCrime/:id', function (req, res) {
-    dbC.run("DELETE FROM crime_offences WHERE id ="+req.params.id+"", function(err, row)
+    var dbC = new sqlite3.Database(file);
+    dbC.run("DELETE FROM crime_offences WHERE id =?", req.params.id, function(err, row)
     {
         // this.changes tells you how many changes were just made
         if (this.changes == 1) {
             result = "Deleted from Crimes DB with id: " +
                 req.params.id + "\n";
             res.send(result);
+            console.log("1 row deleted with ID: " + req.params.id);
         }
         else{
             result = "Could not find record with id: " +
                 req.params.id + "\n";
             res.send(result);
+            console.log("No rows deleted");
         }
     });
+    dbC.close();
 });
 
+//Query to delete a particular record in the Population db by specific id
 app.delete('deletePopulation/:id', function (req, res) {
+    var dbP = new sqlite3.Database(file2);
    dbP.run("DELETE FROM population WHERE id ="+req.params.id+"", function(err, row) {
        if(this.changes == 1) {
            result = "Deleted from Population DB with id:"
                + " " + req.params.id + "\n";
            res.send(result);
+           console.log("1 row deleted with ID: " + req.params.id);
        }
        else{
            result = "Could not find record with id: " +
@@ -158,6 +190,5 @@ app.delete('deletePopulation/:id', function (req, res) {
            res.send(result);
        }
    }); 
+    dbP.close();
 });
-//Start the server.
-var server = app.listen(8080);
